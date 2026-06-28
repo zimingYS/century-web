2c7ea7dd310e03f4fadb2a7787933046751384c6
ziming0v0 <1563238589@qq.com>
2026-06-28 23:36:34 +0800

    添加初始路由和CI工作流

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,16 +4,28 @@ use axum::*;
 async fn main() {
     // 创建网站路由
     let app = Router::new()
-        .route("/", routing::get(root));
+        .route("/", routing::get(home))
+        .route("/about", routing::get(about))
+        .route("/login", routing::post(login));
 
     // 监听端口
-    let linstener = tokio::net::TcpListener::bind(("127.0.0.1", 3000)).await.unwrap();
+    let linstener = tokio::net::TcpListener::bind(("127.0.0.1", 3000))
+        .await
+        .unwrap();
     println!("Server is running at 'http://127.0.0.1:3000'");
 
     // 处理请求响应
     serve(linstener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn home() -> &'static str {
+    "Home Page"
+}
+
+async fn about() -> &'static str {
+    "About Page"
+}
+
+async fn login() -> &'static str {
+    "Login Success"
 }
